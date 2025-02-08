dc448264ff61be61209e5d7e6b869febe5a303c2
Chirag Dahiya <dahiyachirag67@gmail.com>
2025-02-08 23:15:02 +0530

    Completed Proj

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get("/", async (req, res) => {
 app.get("/new", (req, res) => {
   res.render("modify.ejs", { heading: "New Post", submit: "Create Post" });
 });
-
+// Route to editing existing post
 app.get("/edit/:id", async (req, res) => {
   try {
     const response = await axios.get(`${API_URL}/posts/${req.params.id}`);
